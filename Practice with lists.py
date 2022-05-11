8a26c7576ba8b5ae68af013231a90b8472f6e537
royaj324 <98133288+royaj324@users.noreply.github.com>
2022-05-10 22:21:22 -0400

    Add files via upload

---
+++ Practice with lists.py
@@ -0,0 +1,15 @@
+a = [1, -7, 0, 0, 5, 10, 53, "does this work"]
+a[0] = 99
+a[2] = 0
+a[4] = "your mother"
+b = a
+print(b)
+c = int(a[0])
+d = c + 1
+print(d)
+## WOOOOOOOOOOOOOOOOOOHHHHHHH
+a[1] = (1, 2)
+print(a)
+print("my favorite number is", b[6])
+
+
